8b5455a9365d90b0a9e43896ec61920e2d0cc97c
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-04 00:25:33 -0300

    feat: páginas de login e registro personalizadas

---
+++ Recape/Controllers/AgendamentosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recape.Controllers
+{
+    public class AgendamentosController : Controller
+    {
+        [Authorize]
+        public IActionResult ListarAgendamentos()
+        {
+            return View();
+        }
+    }
+}
--- Recape/Controllers/HomeController.cs
+++ Recape/Controllers/HomeController.cs
@@ -19,11 +19,6 @@ namespace Recape.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
