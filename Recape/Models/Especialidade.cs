22cd166e705bb2182e681e081e75eec58d50ec9d
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-04 21:10:31 -0300

    feat: esboço entidades para consulta

---
+++ Recape/Models/Especialidade.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Recape
+{
+    public class Especialidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Recape/Models/Medico.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recape
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Especialidade Especialidade { get; set; }
+    }
+}
