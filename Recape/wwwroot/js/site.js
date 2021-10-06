faba782ad47ce258b18729baeb33fc25d2b3c7eb
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-06 18:33:58 -0300

    feat: concluído desenho da página inicial

--- Recape/wwwroot/js/site.js
+++ Recape/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 900);
+});
