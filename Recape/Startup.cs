5a949121ff8e7e5b9c9f72885e0be416dac4b316
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-09 16:15:14 -0300

    feat: remover páginas de gerenciamento de conta

--- Recape/Startup.cs
+++ Recape/Startup.cs
@@ -45,6 +45,7 @@ namespace Recape
             );
 
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
