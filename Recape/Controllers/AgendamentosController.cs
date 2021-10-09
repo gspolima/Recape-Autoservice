83125cfa0afe8da81d01c1c6ce3169b5e5b2a3f2
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-09 19:03:43 -0300

    feat: teste com UserManager e ViewModel

--- Recape/Controllers/AgendamentosController.cs
+++ Recape/Controllers/AgendamentosController.cs
@@ -1,14 +1,29 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Recape.ViewModels;
 
 namespace Recape.Controllers
 {
     public class AgendamentosController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AgendamentosController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         [Authorize]
         public IActionResult ListarAgendamentos()
         {
-            return View();
+            var userId = userManager.GetUserId(User);
+            var viewModel = new AgendamentosViewModel()
+            {
+                UserId = userId
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Recape/ViewModels/AgendamentosViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Recape.ViewModels
+{
+    public class AgendamentosViewModel
+    {
+        public string UserId { get; set; }
+    }
+}
