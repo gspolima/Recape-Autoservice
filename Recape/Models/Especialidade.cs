db11c499bd1ec2ba5005866ddb59af62bc58b1da
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-10 20:10:39 -0300

    feat: remover lista de medicos da especialidade

--- Recape/Models/Especialidade.cs
+++ Recape/Models/Especialidade.cs
@@ -1,12 +1,8 @@
-using System.Collections.Generic;
-
 namespace Recape.Models
 {
     public class Especialidade
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-
-        public List<Medico> Medicos { get; set; }
     }
 }
