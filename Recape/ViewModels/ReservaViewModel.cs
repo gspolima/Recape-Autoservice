e3cbf0a37d306cce4fe9eb58c8615223abeaaeb9
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-11 00:10:24 -0300

    feat: listar reservar existentes para um usuário

---
+++ Recape/ViewModels/ReservaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Recape.Models;
+
+namespace Recape.ViewModels
+{
+    public class ReservaViewModel
+    {
+        public int Id { get; set; }
+        public IdentityUser Cliente { get; set; }
+        public Poltrona Poltrona { get; set; }
+        public string DataPartida { get; set; }
+        public string HorarioPartida { get; set; }
+    }
+}
