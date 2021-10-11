c9e6d5ffd1a914d4872ea1a0323ff8db5f34edaf
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-11 00:08:16 -0300

    feat: adicionar repositório de dados para reservas

---
+++ Recape/Data/Repository/IReservaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Recape.Models;
+using System.Linq;
+
+namespace Recape.Data.Repository
+{
+    public interface IReservaRepository
+    {
+        IQueryable<Reserva> GetReservas(string usuarioId);
+
+        int CriarReserva(Reserva reserva);
+    }
+}
---
+++ Recape/Data/Repository/ReservaRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recape.Models;
+using System.Linq;
+
+namespace Recape.Data.Repository
+{
+    public class ReservaRepository : IReservaRepository
+    {
+        private readonly RecapeDbContext dbContext;
+
+        public ReservaRepository(RecapeDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public int CriarReserva(Reserva reserva)
+        {
+            dbContext.Add(reserva);
+            var registrosCriados = dbContext.SaveChanges();
+            return registrosCriados;
+        }
+
+        public IQueryable<Reserva> GetReservas(string usuarioId)
+        {
+            var results = dbContext.Reservas
+                .Where(r => r.ClienteId == usuarioId)
+                .Include(r => r.Cliente)
+                .Include(r => r.Poltrona)
+                .Include(r => r.Poltrona.Viagem);
+
+            return results;
+        }
+    }
+}
