dc2619ae680c000664d18649438d32f18a3e9652
Gustavo Sampaio <sampaioglima@gmail.com>
2021-10-17 17:29:21 -0300

    feat: criar action que dispara inserção das poltronas

---
+++ Recape/Controllers/ViagensController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recape.Data.Repository;
+using Recape.ViewModels;
+
+namespace Recape.Controllers
+{
+    public class ViagensController : Controller
+    {
+        private readonly IViagemRepository viagemRepository;
+
+        public ViagensController(IViagemRepository viagemRepository)
+        {
+            this.viagemRepository = viagemRepository;
+        }
+
+        public IActionResult InserirPoltronas()
+        {
+            var viewModel = new InserirPoltronasViewModel()
+            {
+                bancoDeDadosSemeado = true,
+            };
+
+            var poltronasInseridas = viagemRepository.InserirPoltronas();
+
+            if (poltronasInseridas != 0)
+            {
+                viewModel.bancoDeDadosSemeado = false;
+                viewModel.sucessoOperacao = true;
+                viewModel.poltronasInseridas = poltronasInseridas;
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Recape/ViewModels/InserirPoltronasViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Recape.ViewModels
+{
+    public class InserirPoltronasViewModel
+    {
+        public bool sucessoOperacao { get; set; }
+        public bool bancoDeDadosSemeado { get; set; }
+        public int poltronasInseridas { get; set; }
+    }
+}
